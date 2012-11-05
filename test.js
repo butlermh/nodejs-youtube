a9a0d432a2aaee5d1f067d457c5523376989b8a0
Franklin van de Meent <fr@nkl.in>
2012-11-05 23:56:32 +0100

    Added test.js

---
+++ test.js
@@ -0,0 +1,57 @@
+/*
+Name:         youtube-feeds - test.js
+Description:  Test script for youtube-feeds module validation
+Source:       https://github.com/fvdm/nodejs-youtube
+Feedback:     https://github.com/fvdm/nodejs-youtube/issues
+License:      Unlicense / Public Domain
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
+*/
+
+var youtube = require('./'),
+    errors = 0
+
+function doTest( err, label, tests ) {
+	if( err instanceof Error ) {
+		console.log( label +': ERROR' )
+		console.log( err, err.stack )
+		errors++
+	} else {
+		var testErrors = []
+		tests.forEach( function( test ) {
+			if( test[1] !== test[2] ) {
+				testErrors.push(test[0])
+			}
+		})
+		
+		if( testErrors.length == 0 ) {
+			console.log( label +': ok' )
+		} else {
+			console.log( label +': failed ('+ testErrors.join(', ') +')' )
+			errors++
+		}
+	}
+}
